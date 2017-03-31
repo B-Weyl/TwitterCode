72edb8eb6d52fdf4f05d3330b7ea491823a43bf0
bweyl <btw2292@gmail.com>
2017-03-31 15:24:23 -0400

    first commit

---
+++ cloud.py
@@ -0,0 +1,31 @@
+from wordcloud import WordCloud, STOPWORDS
+from os import path
+import matplotlib.pyplot as plt
+
+def cloud(textfile):
+    d = path.dirname(__file__)
+    text = open(path.join(d, textfile + '.txt'), encoding='UTF-8').read()
+    filter_title = textfile
+    stopwords = set(STOPWORDS)
+    stopwords.add("RT")
+    stopwords.add("CO")
+    stopwords.add("HTTPS")
+    stopwords.add("AMP")
+    stopwords.add("RETWEET")
+    stopwords.add(filter_title)
+    wordcloud = WordCloud(max_font_size=60, stopwords=stopwords)
+    wordcloud.generate(text)
+    stopwords.remove(filter_title)
+    plt.figure()
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis("off")
+    plt.show()
+
+
+def main():
+    cloud(textfile='password')
+
+
+if __name__ == '__main__':
+    main()
+
