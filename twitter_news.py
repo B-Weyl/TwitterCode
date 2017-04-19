13f6a1517fff242eaba003d4f12d5f44540a20f5
Brian Weyl <bweyl@Brians-MacBook-Pro.local>
2017-04-19 07:33:34 -0400

    More programs

--- twitter_news.py
+++ twitter_news.py
@@ -1,30 +1,46 @@
 import tweepy
 import secrets
 import argparse
-import matplotlib as plt
 
 
 auth = tweepy.OAuthHandler(secrets.consumer_key, secrets.consumer_secret)
 auth.set_access_token(secrets.access_token, secrets.access_token_secret)
 twitter_api = tweepy.API(auth)
 parser = argparse.ArgumentParser()
-parser.add_argument("-w", "--woeid", required=True, help="the woeid of where trends should come from")
+parser.add_argument("-w", "--woeid", help="the woeid of where trends should come from")
 parser.add_argument("-t", "--top", default=10, type=int, help="the number of trends to get: default is 10")
+parser.add_argument("-l", "--location", default='Philadelphia', type=str, help="the location of trends you would like to get")
 parser.add_argument("-i", "--international", required=False, help="include international trends (non-usa)")
+# parser.add_argument("-r", "--range", required=False, help="determines in how many places a trend is trending")
 args = parser.parse_args()
 
-print(args.woeid)
-print(args.top)
+# print(args.woeid)
+# print(args.top)
 
 
 def get_woeid_usa():
-    trends = twitter_api.trends_available()
-    all_woeids = {trend['name']: trend['woeid'] for trend in trends}
-    usa_woeids = {trend['name']: trend['woeid'] for trend in trends if trend['country'] == 'United States'}
+    places = twitter_api.trends_available()
+    all_woeids = {place['name'].lower(): place['woeid'] for place in places}
+    usa_woeids = {place['name'].lower(): place['woeid'] for place in places if place['country'] == 'United States'}
     if args.international:
         return all_woeids
     else:
         return usa_woeids
+
+
+# def get_trend_range(trend):
+#     """
+#     given a trend, return all of the woeids with that trend
+#     :param woeid:
+#     :return:
+#     """
+#     places = twitter_api.trends_available()
+#     all_woeids = {place['name']: place['woeid'] for place in places}
+#     trending_places = []
+#     for value in all_woeids.values():
+#         if trend in twitter_api.trends_place(value):
+#             trending_places.append(value)
+#     return trending_places
 
 
 def trends_and_volumes(woeid):
@@ -32,28 +48,36 @@ def trends_and_volumes(woeid):
     create a dict of trends from a certain woeid, and tweet_volume
     :return: dict
     """
-    woeid = args.woeid
+    if args.woeid:
+        woeid = args.woeid
+    else:
+        woeid = location_to_woeid(args.location)
     trending_topics = []
     tweet_volume = []
-    if woeid.isnumeric():
-        woeid_trends = twitter_api.trends_place(woeid)
-        for woeid_trend in woeid_trends:
-            for x in range(args.top):
-                trending_topics.append(woeid_trend['trends'][x]['name'])
-                tweet_volume.append(woeid_trend['trends'][x]['tweet_volume'])
+    woeid_trends = twitter_api.trends_place(woeid)
+    for woeid_trend in woeid_trends:
+        for x in range(args.top):
+            trending_topics.append(woeid_trend['trends'][x]['name'])
+            tweet_volume.append(woeid_trend['trends'][x]['tweet_volume'])
     trend_volume_dict = dict(zip(trending_topics, tweet_volume))
     return trend_volume_dict
 
 
 def location_to_woeid(location):
+    location = location.lower()
     trends = twitter_api.trends_available()
     error_string = "This location cannot be resolved to a WOEID"
-    all_woeids = {trend['name']: trend['woeid'] for trend in trends}
+    all_woeids = get_woeid_usa()
     if location in all_woeids:
         return all_woeids[location]
     else:
-        return error_string
-    
+        # print('This is a list of all available locations to get trends from:')
+        # names = []
+        # for trend in trends:
+        #     names.append(trend['name'])
+        # print(names)
+        raise ValueError('This location cannot be resolved to a WOEID')
+
 
 def main():
     woeids = get_woeid_usa()
---
+++ twitter_overflow.py
@@ -0,0 +1,12 @@
+import tweepy
+import secrets
+
+auth = tweepy.OAuthHandler(secrets.consumer_key, secrets.consumer_secret)
+auth.set_access_token(secrets.access_token, secrets.access_token_secret)
+twitter_api = tweepy.API(auth)
+
+tweets = twitter_api.user_timeline()
+username = 'Bweyl_'
+limit = 10
+for status in tweepy.Cursor(twitter_api.user_timeline, screen_name=username).items(limit):
+    print(str(status.place))
