277f1b8c0f14349b28746ff3dc62f71022f757e0
B-Weyl <brian.t.weyl@gmail.com>
2017-05-21 22:16:03 -0400

    Delete retweets.py

--- retweets.py
+++
@@ -1,9 +0,0 @@
-import tweepy
-import secrets
-
-
-auth = tweepy.OAuthHandler(secrets.consumer_key, secrets.consumer_secret)
-auth.set_access_token(secrets.access_token, secrets.access_token_secret)
-twitter_api = tweepy.API(auth)
-
-print(twitter_api.search(q='snowflake', rpp=1))
