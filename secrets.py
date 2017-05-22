ba5ae15e68c065ffd6693bc44b856df290b80f06
B-Weyl <brian.t.weyl@gmail.com>
2017-05-21 22:13:00 -0400

    Delete secrets.py

--- secrets.py
+++
@@ -1,5 +0,0 @@
-consumer_key = "X7tJHPPLOCtFVMjhj3Um0o15Y"
-consumer_secret = "13wuea77HTJiNTAqp1SDiGRfh9eRaglnqSP0aTFayzCJTysLIm"
-access_token = "201417789-EJNCq78NgygMRMLWQFCC2jWj6JlLKi9EMZn7xgMe"
-access_token_secret = "gKFXbbw7RDelAXcFJo7JTbiTwqttIfP7THqlsIjNJ0Niz"
-
